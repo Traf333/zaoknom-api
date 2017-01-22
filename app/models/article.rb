6879d7469ed137e92b06a7f88591f729b25a440f
Traf333 <igortr333@gmail.com>
2017-01-22 09:28:32 +0800

    update published

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
 
   scope :published, -> { where.not(published_at: nil) }
+
+
+  def published=(value)
+    self.published_at = value == 'true' ? Time.now : nil
+  end
 end
